ce324d0157e27732a1d9cd654f274694d7cd9e89
Jordan Sheldon <Jordanhsheldon@gmail.com>
2022-01-18 12:01:01 -0500

    added 3 new html files  and edited app.py to access them

--- covid_dasboard/app.py
+++ covid_dasboard/app.py
@@ -8,3 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
